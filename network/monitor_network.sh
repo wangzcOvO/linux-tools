19dcde2fe55ba86f61853d3aedfd80d13cb32aee
wangzcOvO <wzhengchang@vip.qq.com>
2023-11-14 17:06:35 +0800

    feat: support input specific NIC, default to all
    first arg to the specific NIC(Network Interface Card)

--- network/monitor_network.sh
+++ network/monitor_network.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-NETWORK_INTERFACE="enp2s0"
-
 if ! command -v speedometer &> /dev/null; then
-    echo "安装 speedometer..."
-    sudo apt-get install -y speedometer   #  Debian/Ubuntu
-    # sudo yum install -y speedometer      #  CentOS/RHEL
+    echo "install speedometer..."
+    sudo apt-get install -y speedometer   # Debian/Ubuntu
+    # sudo yum install -y speedometer      # CentOS/RHEL
 fi
 
-speedometer -r "$NETWORK_INTERFACE" -t "$NETWORK_INTERFACE"
+if [ $# -eq 0 ]; then
 
+    # If no NIC is specified, a list of all nics is obtained
+    network_interfaces=$(ip -o link show | awk -F': ' '{print $2}' | tr '\n' ' ')
+else
+    network_interfaces="$@"
+fi
+
+for interface in $network_interfaces; do
+    args="$args -r $interface -t $interface"
+done
+
+speedometer $args
